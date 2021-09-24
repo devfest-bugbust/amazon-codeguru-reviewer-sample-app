01ec76efd1ef9160770aac70db8fc4728903c1d0
Chazal <chazalf@88665a14a6c3.ant.amazon.com>
2021-09-24 08:27:51 +0200

    refactor

--- src/main/java/com/shipmentEvents/demo/createCookie.java
+++
@@ -1,8 +0,0 @@
-import javax.servlet.http.Cookie;
-
-public static void createCookie() {
-  Cookie cookie = new Cookie("name", "value");
-    cookie.setMaxAge(60 * 60 * 24 * 365);
-    cookie.setPath("/");
-    response.addCookie(cookie);
-}
